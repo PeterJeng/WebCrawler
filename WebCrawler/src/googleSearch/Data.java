9ba9b0fb39bd85023a3afac06e1a4cffee34c534
Kunal <mishra.kunal@gmail.com>
2018-03-24 19:28:17 -0400

    Added Data class and updated GoogleSearchResults file

---
+++ WebCrawler/src/googleSearch/Data.java
@@ -0,0 +1,27 @@
+package googleSearch;
+
+// Class which will hold all the information each value in our data set will contain
+// @Peter feel free to add any functionality you see fit!
+public class Data {
+	private String title;
+	private String url;
+	private String date;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
--- WebCrawler/src/googleSearch/GoogleSearchResults.java
+++ WebCrawler/src/googleSearch/GoogleSearchResults.java
@@ -1,5 +1,7 @@
 package googleSearch;
 
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -27,21 +29,35 @@ public class GoogleSearchResults {
 		// (1), and F2017 is year
 		// modifying date to 365 changes it to december
 		// ccd_max is end date, keep end and start date the same for best results
-		String URL = "https://www.google.com/search?q=cryptocurrency&num=100&biw=1536&bih=759&source=lnt&tbs=sbd%3A1%2Ccdr%3A1%2Ccd_min%3A1%2F1%2F2017%2Ccd_max%3A1%2F1%2F2017&tbm=";
+		String URL = "https://www.google.com/search?q=cryptocurrency&num=100&biw=1536&bih=759&source=lnt&tbs=sbd%3A1%2Ccdr%3A1%2Ccd_min%3A1%2F1%2F2017%2Ccd_max%3A1%2F1%2F2017&tbm=&google_abuse=GOOGLE_ABUSE_EXEMPTION%3DID%3D413f6cad97ec84eb:TM%3D1521932963:C%3Dr:IP%3D24.185.58.176-:S%3DAPGng0tagudTVyJtubNOOi_SBx05IpU0DQ%3B+path%3D/%3B+domain%3Dgoogle.com%3B+expires%3DSun,+25-Mar-2018+02:09:23+GMT";
 		// Fetch the page
+		
+		// ArrayList to Hold the data
+		ArrayList<Data> DataSet = new ArrayList<Data>();
 
 		int count = 0;
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 3; i++) {
+			
+
+// *NOTE: If google flags you for being a robot, you can copy paste the url and submit the form confirming you're not a robot, then it will append some stuff to your previous url which you can use as the new value for the url variable
+// Notice that after 'bm=' there is something called 'Google_Abuse_Exemption', I believe that allows us to continue scraping!
+			
 			URL = "https://www.google.com/search?q=cryptocurrency&num=100&biw=1536&bih=759&source=lnt&tbs=sbd%3A1%2Ccdr%3A1%2Ccd_min%3A1%2F"
-					+ i + "%2F2017%2Ccd_max%3A1%2F" + i + "%2F2017&tbm=";
+					+ i + "%2F2017%2Ccd_max%3A1%2F" + i + "%2F2017&tbm=&google_abuse=GOOGLE_ABUSE_EXEMPTION%3DID%3D413f6cad97ec84eb:TM%3D1521932963:C%3Dr:IP%3D24.185.58.176-:S%3DAPGng0tagudTVyJtubNOOi_SBx05IpU0DQ%3B+path%3D/%3B+domain%3Dgoogle.com%3B+expires%3DSun,+25-Mar-2018+02:09:23+GMT";
 			Document doc = Jsoup.connect(URL).userAgent(USER_AGENT).get();
+			Data newData;
 
 			// Traverse the results
 			for (Element result : doc.select("div.g")) {
 				String title = result.select("h3.r a").text();
 				String url = result.select("h3.r a").attr("href");
 				String date = result.select("span.f").text();
-
+				
+				newData = new Data();
+				newData.setTitle(title);
+				newData.setUrl(url);
+				newData.setDate(date);
+				DataSet.add(newData);
 				// there are video links too, so this gets the date for video links
 				if (date.isEmpty()) {
 					date = result.select("div.slp.f").text();
@@ -53,12 +69,21 @@ public class GoogleSearchResults {
 			
 			//hopefully prevents google from blocking the crawler
 			Thread.sleep(5000);
+			
+// Don't know why adding this print statement here helped continue the program execution but it did so thats cool!
+			System.out.println("HERE");
 
 		}
 
 		// TODO: find how to do next page
 		System.out.println("Pages found: " + count);
+		
+// Wanted to make sure that the data is actually getting stored in the arraylist and it is so now I will look for a way to organize this data into a nice graph
+		System.out.println("Printing data from arrayList");
+		System.out.println("----------------------------");
+		for(Data p : DataSet) {
+			System.out.println(p.getDate() + " | " + p.getTitle() + " | " + p.getUrl());
+		}
 
 	}
-
 }
